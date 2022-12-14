cfa678f5a3800e3bbbf5b4d1006646a08c4f9c1c
princessempel <princess.empel@gmail.com>
2022-12-14 03:08:57 -0500

    add all files

---
+++ UITableView/UITableView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITableView
+//
+//  Created by Princess Empel on 10/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITableView/UITableView/UITableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewApp.swift
+//  UITableView
+//
+//  Created by Princess Empel on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct UITableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
